8054ce409b9ff68b8232e40ffad3baf304fd0806
cody <cody.poloski@gmail.com>
2025-07-06 02:38:20 -0500

    sha256 hashing

--- main.c
+++ main.c
@@ -1,4 +1,7 @@
 #include <time.h>
+#include <openssl/sha.h>
+#include <string.h>
+#include <stdio.h>
 
 typedef struct Block {
     int index;
@@ -10,6 +13,25 @@ typedef struct Block {
     struct Block* next;
 } Block;
 
-int main() {
+void sha256(char *str, char outputBuffer[65]) {
+    unsigned char hash[SHA256_DIGEST_LENGTH];
+    SHA256((unsigned char*)str, strlen(str), hash);
+    // convert to hex and write to output buffer
+    for (int i=0; i<SHA256_DIGEST_LENGTH; i++) {
+        // move write pointer forward by 2 each iteration to 
+        // place each hex pair in the right position
+        sprintf(outputBuffer + (i * 2), "%02x", hash[i]);
+    }
+    outputBuffer[64] = 0;
+}
 
+int main() {
+    char *input = "hello world";
+    char hash_output[65];
+
+    sha256(input, hash_output);
+    printf("Input: %s\n", input);
+    printf("SHA-256: %s\n", hash_output);
+
+    return 0;
 }
